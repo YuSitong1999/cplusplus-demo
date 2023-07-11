9041551f9d12e75e91790f9c32e55bc3c54e15d5
Yu Sitong <yusitong1999@foxmail.com>
2023-07-11 16:06:13 +0200

    2.1 空指针 和 常量表达式

---
+++ 2.1/nullptr.cpp
@@ -0,0 +1,30 @@
+//
+// Created by root on 7/8/23.
+//
+
+#include <iostream>
+#include <type_traits>
+
+void foo(char *);
+void foo(int);
+
+int main() {
+    if (std::is_same<decltype(NULL), decltype(0)>::value)
+        std::cout << "NULL == 0" << std::endl;
+    if (std::is_same<decltype(NULL), decltype((void*)0)>::value)
+        std::cout << "NULL == (void *)0" << std::endl;
+    if (std::is_same<decltype(NULL), std::nullptr_t>::value) // C++11
+        std::cout << "NULL == nullptr" << std::endl;
+
+    foo(0);          // 调用 foo(int)
+    // foo(NULL);    // 该行不能通过编译
+    foo(nullptr);    // 调用 foo(char*) // ++11
+    return 0;
+}
+
+void foo(char *) {
+    std::cout << "foo(char*) is called" << std::endl;
+}
+void foo(int i) {
+    std::cout << "foo(int) is called" << std::endl;
+}
